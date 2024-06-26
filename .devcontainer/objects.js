aa7651aa9873e381215820b2d9f6cb2d228f547e
Ayan51542 <ayan51542@gmail.com>
2024-06-26 15:15:09 +0000

    command config done

---
+++ .devcontainer/objects.js
@@ -0,0 +1,7 @@
+let number =2
+let namee=" 222"
+console.log(number+namee);
+ number= String(number)
+namee=Number(namee)
+console.log("number is of " ,typeof(number));
+console.log("namee is of " ,typeof(namee));
