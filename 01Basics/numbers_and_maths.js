1932ca3e6882d625f502aeb8a8232ec98cbc8ab3
Ayan51542 <ayan51542@gmail.com>
2024-06-28 20:05:17 +0000

    numbers and maths

---
+++ 01Basics/numbers_and_maths.js
@@ -0,0 +1,15 @@
+const number1= new Number(100);
+
+//console.log(number1.toLocaleString().length);
+
+const number2 = new Number(44.4888);
+//console.log(number2.toPrecision(1));
+
+//console.log(number1.valueOf())
+// ++++++++++++++++++ maths ++++++++++++++++++++++++++++
+
+//console.log(Math.PI);
+const newnumber = new Number(44.48);
+//console.log(Math.ceil(newnumber));
+// random numbers from 50-70
+console.log(Math.floor(Math.random()*(70-50))+50);
