0759b640429e692062b23243fa71086c52dfc8a2
Ayan51542 <ayan51542@gmail.com>
2024-06-28 10:58:25 +0000

    command config done

---
+++ 01Basics/strings.js
@@ -0,0 +1,8 @@
+const string1= new String('hello my name is Ayan');
+console.log(`This is string 1 ${string1} `);
+const string2=string1.split(' ');
+console.log(string2);
+const string3=string1.substring(0,8);
+console.log(string3);
+const string4=string1.trim();
+console.log(string4);
