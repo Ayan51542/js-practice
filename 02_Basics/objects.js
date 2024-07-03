b9fdc6ad19a32fb7e0eab7b652d275329c4cdbfe
Ayan51542 <ayan51542@gmail.com>
2024-07-03 07:35:03 +0000

    more in objects

--- 02_Basics/objects.js
+++ 02_Basics/objects.js
@@ -31,5 +31,47 @@ const myobj3={...myobj4,...myobj2}
 const obj5=Object.assign({},myobj,myobj2,myobj4)
 //console.log(obj5);
 //if the array of objects arises lets suppose we have users name array an in which all the contents are objects so we'll do Object Users[1].email="hehe@gmail.com"
-console.log(Object.keys(myobj4));
-console.log(myobj4.hasOwnProperty("Karachi"));
+//console.log(Object.keys(myobj4));
+//console.log(myobj4.hasOwnProperty("Karachi"));
+const mySym = Symbol("key1")
+
+
+const JsUser = {
+    name: "Hitesh",
+    "full name": "Hitesh Choudhary",
+    [mySym]: "mykey1",
+    age: 18,
+    location: "Jaipur",
+    email: "hitesh@google.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+// console.log(JsUser.email)
+ //console.log(JsUser["email"])
+//console.log(JsUser["full name"])
+// console.log(JsUser)
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+console.log(instructor);
+
+// {
+//     "name": "hitesh",
+//     "coursename": "js in hindi",
+//     "price": "free"
+// }
+
+/*[
+    {},
+    {},
+    {}
+]*/
